50580dfd14c192950361140238ebfdf457bf0ca7
Samsung <Samsung@Samsung-PC>
2020-03-11 09:41:06 +0530

    first commit

---
+++ Rajini/src/main/java/org/venky/Rajini/App.java
@@ -0,0 +1,13 @@
+package org.venky.Rajini;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
